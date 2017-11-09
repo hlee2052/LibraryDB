ddd82b8f114a7424fb8ef28fcfd7eeb159d0b38c
Rescobvr <jmah@Bruce-Waynes-MacBook-Pro.local>
2017-11-08 19:45:10 -0800

    added some content and colors to the database

--- libdatabase.php
+++ libdatabase.php
@@ -1,2 +1,20 @@
-<p> Library Database </p>
-<p> testing to see if this works </>
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title> Library Database </title>
+    <link rel="stylesheet" type="text/css" href="style.php"/>
+  </head>
+  <body>
+    <style>
+    body { background-color: Ivory}
+  </style>
+    <div class="title_db">
+      <p> Library Database </p>
+    </div>
+    <p> reset table </p>
+    <p><input type="submit" value="Reset" name="reset"></p>
+    </form>
+
+  </body>
+  </html>
---
+++ style.php
@@ -0,0 +1,10 @@
+<?php
+  header('Content-type: text/css; charset:UTF-8');
+ ?>
+
+.title_db{
+  font-size: 32pt;
+  color: black;
+  font-weight: bold;
+  font-family: "Georgia", Times, serif;
+}
