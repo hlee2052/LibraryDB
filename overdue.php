8f9e41cd38befbf79263e5185e84e9052d1fcf27
Carol Leung <Carol@dhcp-128-189-210-21.ubcsecure.wireless.ubc.ca>
2017-11-14 12:31:58 -0800

    started sql code for signin and overdue

--- overdue.php
+++ overdue.php
@@ -9,7 +9,6 @@
 
   <body>
     <div class="menu">
-      <!--can't get this to work for some reason... < ?php require 'menu.php';?>-->
       <a href="staff.php">Home</a>
       <a href="overdue.php">Overdue</a>
       <a href="event.php">events</a>
@@ -23,48 +22,53 @@
   </style>
 
 <!--a div class set up for the database heading-->
-    <div>
-      <h1> Overdue media </h1>
+  <h1> Overdue media </h1>
 
-      <table>
-        <h2>Books:</h2>
-        <tr>
-          <th>mediaid</th>
-          <th>booktitle</th>
-          <th>reserved</th>
-          <th>borrow date</th>
-          <th>days overdue</th>
-          <th>borrowed by</th>
-        </tr>
-        <tr>
-          <td>ugh pls assume data is here</td>
-        </tr>
-      </table>
+    <?php
+    include "phpfunctions.php";
+    function printResult($type, $result) { //prints results from a select statement
+      echo "<h2>" . $type . "</h2>";
+    	echo "<table style='border:2px solid red'>";
+    	echo "<tr>
+      <th style='border:1px solid black'>ID</th>
+      <th style='border:1px solid black'>Name</th>
+      <th style='border:1px solid black'>Borrowed by</th>
+      <th style='border:1px solid black'>Borrow date</th>
+      <th style='border:1px solid black'>Days overdue</th>
+      <th style='border:1px solid black'>Reserved</th>
+      </tr>";
+    	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+    		echo "<tr>
+        <td style='border:1px solid red'>" . $row["MEDIAID"] . "</td>
+        <td style='border:1px solid red'>" . $row["NAME"] . "</td>
+        <td style='border:1px solid red'>" . $row["MID"] . "</td>
+        <td style='border:1px solid red'>" . $row["BORROWDATE"] . "</td>
+        <td style='border:1px solid red'>" . $row["DAYSOVERDUE"] . "</td>
+        <td style='border:1px solid red'>" . $row["RESERVED"] . "</td>
+        </tr>";
+    	}
+    	echo "</table>";
+    }
 
-      <table>
-        <h2>DVD:</h2>
-        <tr>
-          <th>mediaid</th>
-          <th>dvdtitle</th>
-          <th>reserved</th>
-          <th>borrow date</th>
-          <th>days overdue</th>
-          <th>borrowed by</th>
-        </tr>
-      </table>
+    // Connect Oracle...
+    if ($db_conn) {
+      //GET BOOKS
+      $booklist = executePlainSQL("select * from tab1");
+    	printResult("Book", $booklist);
+      //GET DVDS
+      $dvdlist = executePlainSQL("select * from tab1");
+    	printResult("DVD", $dvdlist);
+      //GET EQUIPMENT
+      $equiplist = executePlainSQL("select * from tab1");
+    	printResult("Equipment", $equiplist);
 
-      <table>
-        <h2>equipment:</h2>
-        <tr>
-          <th>mediaid</th>
-          <th>equipmentname</th>
-          <th>reserved</th>
-          <th>borrow date</th>
-          <th>days overdue</th>
-          <th>borrowed by</th>
-        </tr>
-      </table>
-
-    </div>
+    	//Commit to save changes...
+    	OCILogoff($db_conn);
+    } else {
+    	echo "cannot connect";
+    	$e = OCI_Error(); // For OCILogon errors pass no handle
+    	echo htmlentities($e['message']);
+    }
+    ?>
   </body>
 </html>
--- signin.php
+++ signin.php
@@ -17,51 +17,89 @@
 <!--a div class set up for the database heading-->
     <div class="title_db" align="center">
       <h1> Sign In </h1>
-      <div class="staff">
-        <h2> Sign in as staff </h2>
-        <form="staff">
-          <select>
-            <option>staff</option>
-            <option>name</option>
-            <option>queried</option>
-            <option>from</option>
-            <option>database</option>
-          </select>
-          <input type="submit" value="Sign in as staff"></input>
-          submit should redirect the page to <a href="staff.php"> staff page </a>
-        </form>
-      </div>
-
-
-
-      <div class="member">
-        <h2> Sign in as member </h2>
-        <form="member">
-          <select>
-            <option>member</option>
-            <option>name</option>
-            <option>queried</option>
-            <option>from</option>
-            <option>database</option>
-          </select>
-          <input type="submit" value="Sign in as member"></input>
-          submit should redirect the page to <a href="member.php"> member page </a>
-        </form>
-      </div>
-
-
 
       <div class="new">
         <h2> Sign up </h2>
-        <form="member">
+        <form action="signin.php" method="post">
           <input type="text" name="name" placeholder="First name, Last name"></input>
           <input type="text" name="phone" placeholder="phone"></input>
           <input type="text" name="email" placeholder="email"></input>
           <input type="text" name="address" placeholder="address"></input>
-          <input type="submit" value="Sign up"></input>
+          <input type="submit" value="Sign up" name="new"></input>
         </form>
       </div>
-    </div>
 
+    <?php
+    include "phpfunctions.php";
+    function printResult($type, $result) { //prints results from a select statement
+      echo
+      '<div class="' . $type . '">
+        <h2> Sign in as ' . $type . ' </h2>
+        <form action="' . $type . '.php" method="post">
+          <select>';
+
+      while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+        echo "<option>" . $row["NID"] . "</option>";
+      }
+
+      echo '</select>
+          <input type="submit" value="Sign in as ' . $type . '" name="' . $type . '"></input>
+        </form>
+      </div>';
+    }
+
+    // Connect Oracle...
+    if ($db_conn) {
+      //Staff
+      $stafflist = executePlainSQL("select * from tab1");
+      printResult("staff", $stafflist);
+      //Member
+      $memberlist = executePlainSQL("select * from tab1");
+      printResult("member", $memberlist);
+    	if (array_key_exists('member', $_POST)) {
+    			//Getting the values from user and insert data into the table
+    			$tuple = array (
+    				":bind1" => $_POST['insNo'],
+    				":bind2" => $_POST['insName']
+    			);
+    			$alltuples = array (
+    				$tuple
+    			);
+    			executeBoundSQL("insert into tab1 values (:bind1, :bind2)", $alltuples);
+    			OCICommit($db_conn);
+
+    		} else
+    			if (array_key_exists('new', $_POST)) {
+    				// Update tuple using data from user
+    				$tuple = array (
+    					":bind1" => $_POST['oldName'],
+    					":bind2" => $_POST['newName']
+    				);
+    				$alltuples = array (
+    					$tuple
+    				);
+    				executeBoundSQL("update tab1 set name=:bind2 where name=:bind1", $alltuples);
+    				OCICommit($db_conn);
+
+    		}
+//check if i really need this. does it reload without this?
+    	if ($_POST && $success) {
+    		//POST-REDIRECT-GET -- See http://en.wikipedia.org/wiki/Post/Redirect/Get
+    		header("location: oracle-test.php");
+    	} else {
+    		// Select data...
+    		$result = executePlainSQL("select * from tab1");
+    		printResult($result);
+    	}
+
+    	//Commit to save changes...
+    	OCILogoff($db_conn);
+    } else {
+    	echo "cannot connect";
+    	$e = OCI_Error(); // For OCILogon errors pass no handle
+    	echo htmlentities($e['message']);
+    }
+    ?>
+    </div>
   </body>
 </html>
