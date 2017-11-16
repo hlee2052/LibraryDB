fdbe2708c5494f7493863b4de38964081a241e22
Rescobvr <jmah@dhcp-128-189-230-250.ubcsecure.wireless.ubc.ca>
2017-11-16 12:17:18 -0800

    fixed event page to only insert an event

--- event.php
+++ event.php
@@ -10,10 +10,9 @@
   <body>
     <div class="menu">
       <!--can't get this to work for some reason... < ?php require 'menu.php';?>-->
-      <a href="staff.php">Home</a>
-      <a href="overdue.php">Overdue</a>
-      <a href="event.php">events</a>
-      <a href="signin.php">Sign out</a>
+      <a href="menu.php">Menu</a>
+      <a href="member.php">Member Page</a>
+      <a href="media_member.php">Search Media</a>
     </div>
 
 
@@ -25,19 +24,16 @@
 <!--a div class set up for the database heading-->
       <h1> Event </h1>
 
-      <p>If you wish to reset the table, press the reset button. If this is the first time you're running this page, you MUST use reset</p>
+      <!-- <p>If you wish to reset the table, press the reset button. If this is the first time you're running this page, you MUST use reset</p>
       <form method="POST" action="event.php">
       <p><input type="submit" value="Reset" name="reset"></p>
-      </form>
-
-
-        <h2> Create new event </h2>
+      </form> -->
+        <h2> Create New Event </h2>
         <form method ="POST" action="event.php">
-          <input type="text" name="eventID" placeholder="event ID"></input>
-          <input type="text" name="eventName" placeholder="event name"></input>
-          <input type="text" name="datevar" placeholder="date of event"></input>
+          <input type="text" name="eid" placeholder="event ID"></input>
           <input type="text" name="startTime" placeholder="start time of event"></input>
           <input type="text" name="endTime" placeholder="end time of event"></input>
+          <input type="text" name="ename" placeholder="event name"></input>
           <input type="submit" value="insert" name="insertsubmit"></input>
         </form>
   </body>
@@ -103,48 +99,34 @@ function printResult($result){
   echo "<br><h2>List of Events</h2><br>";
 	echo "<table style='border:2px solid black'>";
 	echo "<tr>
-  <th style='border:1px solid black'>Name</th>
-  <th style='border:1px solid black'>Date</th>
+<th style='border:1px solid black'>Event ID</th>
   <th style='border:1px solid black'>Start Time</th>
   <th style='border:1px solid black'>End Time</th>
+  <th style='border:1px solid black'>Name</th>
   </tr>";
 
   while ($row = OCI_Fetch_Array($result, OCI_BOTH)){
-    echo "<tr><td style='border:1px solid red'>" . $row["NAME"] . "</td>
-    <td style='border:1px solid red'>" . $row["DATEVAR"] . "</td>
+    echo "<tr><td style='border:1px solid red'>" . $row["EID"] . "</td>
     <td style='border:1px solid red'>" . $row["STARTTIME"] . "</td>
     <td style='border:1px solid red'>" . $row["ENDTIME"] . "</td>
+    <td style='border:1px solid red'>" . $row["ENAME"] . "</td>
     </tr>";
   }
   echo "</table>";
 }
 
 if ($db_connection){
-
-  if (array_key_exists ('reset', $_POST)){
-    echo "<br> dropping table <br>";
-    executePlainSQL("drop table event");
-
-    //creating new event table
-    echo "<br> creating new event table <br>";
-    executePlainSQL("create table event (eid number, name varchar2(30), datevar DATE,
-    startTime number, endTime number, primary key (eid))");
-    OCICommit($db_connection);
-
-  } else
     if (array_key_exists('insertsubmit', $_POST)){
       $tuple = array (
-        ":bind1" => $_POST['eventID'],
-        ":bind2" => $_POST['eventName'],
-        ":bind3" => $_POST['datevar'],
-        ":bind4" => $_POST['startTime'],
-        ":bind5" => $_POST['endTime']
+        ":bind1" => $_POST['eid'],
+        ":bind2" => $_POST['startTime'],
+        ":bind3" => $_POST['endTime'],
+        ":bind4" => $_POST['ename']
       );
       $alltuples = array (
         $tuple
       );
-      executePlainSQL("alter session set NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS'");
-      executeBoundSQL("insert into event values (:bind1, :bind2, TO_DATE(:bind3, 'YYYY-MM-DD HH24:MI:SS'), :bind4, :bind5)", $alltuples);
+      executeBoundSQL("insert into event values (:bind1, :bind2, :bind3, :bind4)", $alltuples);
       OCICommit($db_connection);
     }
   if ($_POST && $success) {
