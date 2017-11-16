b1cee1e6115a6209ad4060a8374dc93ec84b26be
Carol Leung <Carol@dhcp-128-189-209-134.ubcsecure.wireless.ubc.ca>
2017-11-16 11:54:45 -0800

    testing signin

--- signin.php
+++ signin.php
@@ -18,17 +18,6 @@
     <div class="title_db" align="center">
       <h1> Sign In </h1>
 
-      <div class="new">
-        <h2> Sign up </h2>
-        <form action="signin.php" method="post">
-          <input type="text" name="name" placeholder="First name, Last name"></input>
-          <input type="text" name="phone" placeholder="phone"></input>
-          <input type="text" name="email" placeholder="email"></input>
-          <input type="text" name="address" placeholder="address"></input>
-          <input type="submit" value="Sign up" name="new"></input>
-        </form>
-      </div>
-
     <?php
     include "phpfunctions.php";
     function printResult($type, $result) { //prints results from a select statement
@@ -39,7 +28,7 @@
           <select>';
 
       while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-        echo "<option>" . $row["NID"] . "</option>";
+        echo "<option>" . $row["MID"] . $row["NAME"] . "</option>";
       }
 
       echo '</select>
@@ -51,47 +40,33 @@
     // Connect Oracle...
     if ($db_conn) {
       //Staff
-      $stafflist = executePlainSQL("select * from tab1");
-      printResult("staff", $stafflist);
+      $stafflist = "SELECT M.name as NAME, M.mid as ID
+        FROM Staff S, Member M
+        WHERE S.mid = M.mid";
+      printResult("Staff", executePlainSQL($stafflist));
       //Member
-      $memberlist = executePlainSQL("select * from tab1");
-      printResult("member", $memberlist);
-    	if (array_key_exists('member', $_POST)) {
-    			//Getting the values from user and insert data into the table
-    			$tuple = array (
-    				":bind1" => $_POST['insNo'],
-    				":bind2" => $_POST['insName']
-    			);
-    			$alltuples = array (
-    				$tuple
-    			);
-    			executeBoundSQL("insert into tab1 values (:bind1, :bind2)", $alltuples);
-    			OCICommit($db_conn);
-
-    		} else
-    			if (array_key_exists('new', $_POST)) {
-    				// Update tuple using data from user
-    				$tuple = array (
-    					":bind1" => $_POST['oldName'],
-    					":bind2" => $_POST['newName']
-    				);
-    				$alltuples = array (
-    					$tuple
-    				);
-    				executeBoundSQL("update tab1 set name=:bind2 where name=:bind1", $alltuples);
-    				OCICommit($db_conn);
-
-    		}
-//check if i really need this. does it reload without this?
-    	if ($_POST && $success) {
-    		//POST-REDIRECT-GET -- See http://en.wikipedia.org/wiki/Post/Redirect/Get
-    		header("location: oracle-test.php");
-    	} else {
-    		// Select data...
-    		$result = executePlainSQL("select * from tab1");
-    		printResult($result);
-    	}
-
+      $memberlist = "SELECT M.name as NAME, M.mid as ID
+        FROM Member M
+        EXCEPT
+        SELECT M2.mid
+        FROM Staff S, Member M2
+        WHERE S.mid = M2.mid";
+      printResult("Member", executePlainSQL($memberlist));
+    	if (array_key_exists('new', $_POST)) {
+  				// Update tuple using data from user
+  				$tuple = array (
+  					":bind1" => $_POST['email'],
+  					":bind2" => $_POST['phone'],
+            ":bind3" => $_POST['name'],
+            ":bind4" => $_POST['address']
+  				);
+  				$alltuples = array (
+  					$tuple
+  				);
+          $newID = executePlainSQL("SELECT MAX(mid) FROM Member";) + 1;
+  				executeBoundSQL("INSERT INTO Member VALUES(105, 0, :bind1, :bind2, :bind3, :bind4)", $alltuples);
+  				OCICommit($db_conn);
+  		}
     	//Commit to save changes...
     	OCILogoff($db_conn);
     } else {
@@ -100,6 +75,17 @@
     	echo htmlentities($e['message']);
     }
     ?>
+    </div>
+
+    <div class="new">
+      <h2> Sign up </h2>
+      <form action="signin.php" method="post">
+        <input type="text" name="name" placeholder="First name, Last name"></input>
+        <input type="text" name="phone" placeholder="phone"></input>
+        <input type="text" name="email" placeholder="email"></input>
+        <input type="text" name="address" placeholder="address"></input>
+        <input type="submit" value="Sign up" name="new"></input>
+      </form>
     </div>
   </body>
 </html>
