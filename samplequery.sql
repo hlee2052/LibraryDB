f831523df1a29dd6a61022a45ea4903f9f274546
CarolLeung <carolleung8205@gmail.com>
2017-11-18 20:53:32 -0800

    Delete samplequery.sql

--- samplequery.sql
+++
@@ -1,6 +0,0 @@
-create view BookCatalog(mediaid,booktitle,reserved,availability,locname) as
-	select B.mediaid,B.booktitle,M.reserved,M.availability,L.locname
-	from book B,media M,location L
-	where B.mediaid=M.mediaid and L.lid=M.lid;
-
-select * from BookCatalog;
